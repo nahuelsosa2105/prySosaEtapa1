f3c951b6e4193437c71e3575feb332a6614d608f
Alumno <Alumno@PC25>
2024-04-29 14:14:07 -0300

    Agregar archivos de proyecto.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace prySosaEtapa1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctFirma = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctFirma
+            // 
+            this.pctFirma.BackColor = System.Drawing.Color.White;
+            this.pctFirma.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctFirma.Location = new System.Drawing.Point(39, 35);
+            this.pctFirma.Name = "pctFirma";
+            this.pctFirma.Size = new System.Drawing.Size(390, 322);
+            this.pctFirma.TabIndex = 0;
+            this.pctFirma.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 404);
+            this.Controls.Add(this.pctFirma);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctFirma;
+    }
+}
+
