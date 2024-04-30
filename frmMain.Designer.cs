cab99352176600f26e869b0a1d0643c62544cc77
nahuelsosa2105 <nahuelsosa2105@gmail.com>
2024-04-30 12:51:00 -0300

    Proyecto completado, con las firmas funcionales, y su correcto guardado en un archivo .PNG

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.pctFirma = new System.Windows.Forms.PictureBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnFirmar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,15 +43,47 @@
             this.pctFirma.Size = new System.Drawing.Size(390, 322);
             this.pctFirma.TabIndex = 0;
             this.pctFirma.TabStop = false;
+            this.pctFirma.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseDown);
+            this.pctFirma.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseMove);
+            this.pctFirma.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseUp);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.White;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(487, 270);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(237, 87);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnFirmar
+            // 
+            this.btnFirmar.BackColor = System.Drawing.Color.White;
+            this.btnFirmar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFirmar.Location = new System.Drawing.Point(487, 150);
+            this.btnFirmar.Name = "btnFirmar";
+            this.btnFirmar.Size = new System.Drawing.Size(237, 87);
+            this.btnFirmar.TabIndex = 2;
+            this.btnFirmar.Text = "GUARDAR";
+            this.btnFirmar.UseVisualStyleBackColor = false;
+            this.btnFirmar.Click += new System.EventHandler(this.btnFirmar_Click);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
             this.ClientSize = new System.Drawing.Size(782, 404);
+            this.Controls.Add(this.btnFirmar);
+            this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.pctFirma);
             this.Name = "frmMain";
-            this.Text = "Form1";
+            this.Text = "Firma";
             ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).EndInit();
             this.ResumeLayout(false);
 
@@ -58,6 +92,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pctFirma;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnFirmar;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,112 @@ using System.Windows.Forms;
 namespace prySosaEtapa1
 {
     public partial class frmMain : Form
+
     {
+        public Bitmap guardarFirma;
         public frmMain()
         {
             InitializeComponent();
+            pctFirma.MouseDown += pctFirma_MouseDown;
+            pctFirma.MouseMove += pctFirma_MouseMove;
+            pctFirma.MouseUp += pctFirma_MouseUp;
+
+            guardarFirma = new Bitmap(pctFirma.Width, pctFirma.Height);
+            pctFirma.Image = guardarFirma;
+        }
+
+        private Point ubicacionPrevia;
+        private bool firmando = false;
+
+
+
+       
+
+        private void pctFirma_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                firmando |= true;
+                ubicacionPrevia = e.Location;
+            }
+        }
+
+        private void pctFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (firmando)
+            {
+                using (Graphics firmita = Graphics.FromImage(guardarFirma))
+                {
+                    Pen pen = new Pen(Color.Black, 1);
+                    firmita.DrawLine(pen, ubicacionPrevia, e.Location);
+                }
+                pctFirma.Invalidate();
+                ubicacionPrevia = e.Location;
+
+            }
+        }
+
+        private void pctFirma_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                firmando = true;
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarFirma();
+        }
+
+        public Bitmap firmaGuardada;
+
+        private void btnFirmar_Click(object sender, EventArgs e)
+        {
+            if(guardarFirma != null)
+            {
+                try
+                {
+                    string carpetaImagenesFirmas = Path.Combine(Application.StartupPath, "Imagenes Firmas");
+
+                    if (!Directory.Exists(carpetaImagenesFirmas))
+                    {
+                        Directory.CreateDirectory(carpetaImagenesFirmas);
+                    }
+
+                    string nombreArchivo = $"firma_{DateTime.Now.ToString("yyyy-MM-dd-HH,mm,ss")}.png";
+
+                    string rutaArchivo = Path.Combine(carpetaImagenesFirmas, nombreArchivo);
+
+                    guardarFirma.Save(rutaArchivo, System.Drawing.Imaging.ImageFormat.Png);
+
+                    MessageBox.Show("¡Firma guardada con exito!");
+
+                    LimpiarFirma();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error al guardar la firma" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se registro ninguna firma, vuelva a intenrarlo");
+            }
+        }
+
+        private void LimpiarFirma()
+        {
+            // Limpiar el bitmap que contiene la firma
+            using (Graphics g = Graphics.FromImage(guardarFirma))
+            {
+                g.Clear(Color.White); // Limpia el bitmap con un fondo blanco
+            }
+
+            // Actualizar el PictureBox para mostrar el bitmap limpio
+            pctFirma.Invalidate();
         }
     }
+
+   
 }
